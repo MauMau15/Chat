6547b43cde85f843fbfb63140e6149b6b168fec6
Mauricio Renero <mauriciorenero@gmail.com>
2018-12-03 12:34:59 -0600

    fixing how we serve our chat as index

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const PORT = 3000;
 let app = require('express')()
 let http = require('http').Server(app)
 
-app.get('/',function(req,res){
-    res.send('<h1>Hello</h1>')
-})
 
 http.listen(PORT,function(){
     console.log('listening on' + PORT)
+    console.log(__dirname)
 })
 
 app.get('/',function(req,res){
